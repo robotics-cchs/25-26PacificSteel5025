0a30d148b643f075c0f32c8b2d60d71791da857c
cdeltacchs <robotics@cchsdons.com>
2026-02-10 09:18:42 -0800

    Updated Swerve and NonSwerve
    Updating the Swerve and NonSwerve to work together and bring harmony

---
+++ SwerveWithPathPlanner/src/main/java/frc/robot/commands/KickerUpCommand.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.KickerSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class KickerUpCommand extends Command {
+  private final KickerSubsystem m_kickerSubsystem;
+  
+  /** Creates a new KickerCommand. */
+  public KickerUpCommand(KickerSubsystem subsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_kickerSubsystem = subsystem;
+    addRequirements(m_kickerSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_kickerSubsystem.inc();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
